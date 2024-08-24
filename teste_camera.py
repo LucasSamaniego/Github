f3a1c9a5926ba58bb3f6254974275fed06e8df7d
LucasSamaniego <samaniego444@gmail.com>
2024-08-23 21:33:56 -0300

    Versao_9

--- teste_camera.py
+++ teste_camera.py
@@ -8,10 +8,6 @@ picam2.start()
 # Capturar imagem
 frame = picam2.capture_array()
 
-# Exibir a imagem capturada (opcional)
-cv2.imshow("Imagem Capturada", frame)
-cv2.waitKey(0)  # Pressione qualquer tecla para fechar a janela
-
 # Salvar a imagem capturada
 cv2.imwrite("foto.jpg", frame)
 
