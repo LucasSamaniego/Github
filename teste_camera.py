10ccb5ff677d32681febc1001b872ee1f1dae75a
LucasSamaniego <samaniego444@gmail.com>
2024-08-23 21:28:56 -0300

    Versao_8

--- teste_camera.py
+++ teste_camera.py
@@ -1,16 +1,20 @@
 import cv2
-cam = cv2.VideoCapture(0)
+from picamera2 import Picamera2
 
-if cam.isOpened():
-    print("Camera successfully opened")
-else:
-    print("Camera could not be opened")
+# Inicializar a câmera
+picam2 = Picamera2()
+picam2.start()
 
-while True:
-    success, image = cam.read() 
-    print("success in taking image? ", success)
-    if success and not image.empty():
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        #further code to be excecuted to process the image
-    else:
-        print("problem capturing image")
+# Capturar imagem
+frame = picam2.capture_array()
+
+# Exibir a imagem capturada (opcional)
+cv2.imshow("Imagem Capturada", frame)
+cv2.waitKey(0)  # Pressione qualquer tecla para fechar a janela
+
+# Salvar a imagem capturada
+cv2.imwrite("foto.jpg", frame)
+
+# Liberar a câmera e fechar janelas
+picam2.close()
+cv2.destroyAllWindows()
